d4623a2a19883930cac92d27884322935d94eebd
Sophie Archibald <arc21004@byui.edu>
2025-12-01 10:45:33 -0700

    Added necessary icons for nav bar

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -19,6 +19,8 @@ const MAPPING = {
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
   'list.bullet': 'format-list-bulleted',
+  'person': 'person',
+  'plus': 'add',
 } as IconMapping;
 
 /**
